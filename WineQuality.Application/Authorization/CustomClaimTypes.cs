63cb3eb2e061cf4ffefb632b358360eba0bce713
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2023-06-11 02:03:35 +0300

    Removed unused files

--- WineQuality.Application/Authorization/CustomClaimTypes.cs
+++
@@ -1,9 +0,0 @@
-namespace WineQuality.Application.Authorization;
-
-public static class CustomClaimTypes
-{
-    public const string DoctorId = "DoctorId";
-    public const string PatientId = "PatientId";
-    public const string HospitalAdministratorId = "HospitalAdministratorId";
-    public const string PatientCaretakerId = "PatientCaretakerId";
-}
--- WineQuality.Application/Authorization/CustomRoles.cs
+++ WineQuality.Application/Authorization/CustomRoles.cs
@@ -6,8 +6,4 @@ public static class CustomRoles
     public const string Admin = "Admin";
     public const string Moderator = "Moderator";
     public const string User = "User";
-    public const string Doctor = "Doctor";
-    public const string Patient = "Patient";
-    public const string HospitalAdministrator = "HospitalAdministrator";
-    public const string PatientCaretaker = "PatientCaretaker";
 }
